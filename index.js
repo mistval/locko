4fbbec81f7f216388eccfff8177ec00e322e2836
Randall Schmidt <randallschmidt1@gmail.com>
2019-06-30 15:34:33 +0700

    add jsdoc comments

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const lockPromiseForKey = {};
 const unlockFunctionForKey = {};
 
+/**
+ * Take out a lock. When this function returns (asynchronously),
+ * you have the lock.
+ * @param {string} key - The key to lock on. Anyone else who
+ *   tries to lock on the same key will need to wait for it to
+ *   be unlocked.
+ */
 async function lock(key) {
   if (!lockPromiseForKey[key]) {
     lockPromiseForKey[key] = Promise.resolve();
@@ -14,6 +21,12 @@ async function lock(key) {
   return takeLockPromise;
 }
 
+/**
+ * Release a lock.
+ * @param {string} key - The key to release the lock for.
+ *   The next person in line will now be able to take out
+ *   the lock for that key.
+ */
 function unlock(key) {
   if (unlockFunctionForKey[key]) {
     unlockFunctionForKey[key]();
